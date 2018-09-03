38b41979a1d28bfd664f691f69e19a68c0883129
beatrizelena <ortizwebdevelopment@gmail.com>
2018-09-03 12:33:16 -0400

    Fix 'See One Session' Make style changes and additions

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -3,14 +3,14 @@ const store = require('./store.js')
 const onSignUpSuccess = function(data) {
   $('#message').html('')
   $("#message").append('Account Created. Log in to use Interpreter Assistant.').show()
-    .delay(2000).fadeOut()
+    .delay(1500).fadeOut()
   $(".form1").css("display", "none")
   }
 
 const onSignInSuccess = function(data) {
   $('#message').html('')
   $("#message").append('Welcome to your Interpreter Assistant').show()
-    .delay(2000).fadeOut()
+    .delay(1500).fadeOut()
   $(".form1").css("display", "none")
   $(".form2").css("display", "none")
   $(".show-after-login").css("display", "flex")
@@ -21,13 +21,13 @@ const onSignInSuccess = function(data) {
 const onChangePwSuccess = function() {
   $('#message').html('')
   $("#message").append('Password change was successful').show()
-    .delay(2000).fadeOut()
+    .delay(1500).fadeOut()
 }
 
 const onSignOutSuccess = function() {
   $('#message').html('')
   $("#message").append('User Logged out. Thank you for using Interpreter Assistant! Come back soon!').show()
-    .delay(2000).fadeOut() 
+    .delay(1500).fadeOut() 
   $(".show-after-login").css("display", "none")
   $(".form1").css("display", "flex")
   $(".form2").css("display", "flex")
@@ -36,14 +36,31 @@ const onSignOutSuccess = function() {
 // Doctor UI: See all doctors (ToDo: By clinic)
   const onGetAllDoctorsSuccess = function(data) {
     $('#show-data').html('')
+    const showTableHeaders = (`
+      <table>
+      <tr>
+        <th>Provider's Name</th>
+        <th>Provider's Phone</th>
+        <th>Clinic</th>
+        <th>Id number</th>
+      </tr>
+      </table>
+      `)
+      $("#show-data").append(showTableHeaders).show()
     for (let i = 0; i < data.doctors.length; i++) {
       // console.log(data)
-      // console.log(data.doctors)
-      // console.log(data.doctors.clinicReference)
-      // console.log(data.doctors.clinic.name)
+      console.log(data.doctors)
+      console.log(data.doctors[i].clinic)
+      // console.log(data.doctors[i].clinic.name)
       const showHTML = (`
-      <p>${data.doctors[i].first_name} ${data.doctors[i].last_name}, ${data.doctors[i].title}, ${data.doctors[i].phone} || Id: ${data.doctors[i]._id}<br />
-      <hr>
+      <table>
+        <tr>
+          <td>${data.doctors[i].first_name} ${data.doctors[i].last_name}, ${data.doctors[i].title}</td>
+          <td>${data.doctors[i].phone}</td>
+          <td>${data.doctors[i].clinic.name} (${data.doctors[i].clinic.abbreviation})</td>
+          <td>${data.doctors[i]._id}</td>
+        </tr>
+      </table>
       `)
       $("#show-data").append(showHTML).show()
   }
@@ -59,30 +76,38 @@ const onSignOutSuccess = function() {
 // Sessions UI
 const onGetAllSessionsSuccess = function(data) {
   $('#show-data').html('')
+    const showTableHeaders = (`
+      <table id="sessions-table">
+      <tr>
+        <th>Provider</th>
+        <th>Notes</th>
+        <th>Other Info</th>
+      </tr>
+      </table>
+      `)
+      $("#show-data").append(showTableHeaders).show()
   for (let i = 0; i < data.sessions.length; i++) {
-    // console.log('in loop')
     const sessionString = data.sessions
-    console.log(sessionString)
+    // console.log(sessionString)
     const showHTML = (`
-    <p>Session with ${sessionString[i].doctorReference.first_name} ${sessionString[i].doctorReference.last_name}, ${sessionString[i].doctorReference.title}, ${sessionString[i].doctorReference.phone}<br />
-    My Notes <small>Written on: ${sessionString[i].createdAt}</small>):<br />
-    <small>${sessionString[i].notes}</small></p>
-    <p><small>Session Id: ${sessionString[i].doctorReference._id} || Doctor Id: ${sessionString[i].doctorReference._id}</small></p>
-    `)
-    $("#show-data").append(showHTML).show()
+      <table>
+        <tr>
+          <td>${sessionString[i].doctorReference.first_name} ${sessionString[i].doctorReference.last_name}, ${sessionString[i].doctorReference.title}<br />
+          ${sessionString[i].doctorReference.phone}</td>
+          <td>${sessionString[i].notes}</td>
+          <td>Notes created on: ${sessionString[i].createdAt}<br />
+          Doctor id: ${sessionString[i].doctorReference._id}<br />
+          Session id: ${sessionString[i]._id}</td>
+        </tr>
+      </table>
+      `)
+      $("#show-data").append(showHTML).show()
   }
   // DANGER CODE NEVER GETS HERE
 }
 
 const onCreateOneSessionSuccess = function (data) {
   $('#message').html('')
-  // const showHTML = (`
-  //   <h6>My Notes with <h5>${store.sessions.doctor.first_name} ${store.session.doctor.last_name}, ${store.session.doctor.title}, ${store.session.doctor.phone}</h6>
-  //   <p>Session Created On: ${data.sessions.createdAt}</p>
-  //   <p>${store.sessions.notes}</p>
-  //   <p>Session ID: ${store.sessions.id}, Doctor ID: ${data.session.doctor.id}</p>
-  //   <p>${store.sessions.doctor.disease}</p>    
-  // `)
   $("#message").append('Session successfully created').show()
     .delay(2000).fadeOut()
   // empty values from form fields
@@ -90,16 +115,32 @@ const onCreateOneSessionSuccess = function (data) {
 }
 
 const onSeeOneSessionSuccess = function (data) {
-  $('.modal-body').html('')
-  $("#myModalLabel").html('Successful action')
-  const showHTML = (`
-      <p>My Session Notes for: ${data.session.doctor.first_name} ${data.session.doctor.last_name}, ${data.session.doctor.title}, ${data.session.doctor.phone}<br />
-      <small>Session Created On: ${data.session.createdAt}</small></p>
-      <p><small>${data.session.notes}</small></p>
-      <hr>
-    `)
-  $(".modal-body").html(showHTML)
-  $("#myModal").modal('show')  
+  const sessionString = data.session
+  console.log(data)
+  $('#show-data').html('')
+    const showTableHeaders = (`
+      <table id="sessions-table">
+      <tr>
+        <th>Provider</th>
+        <th>Notes</th>
+        <th>Other Info</th>
+      </tr>
+      </table>
+      `)
+      $("#show-data").append(showTableHeaders).show()
+      const showHTML = (`
+      <table>
+        <tr>
+          <td>${sessionString.doctorReference.first_name} ${sessionString.doctorReference.last_name}, ${sessionString.doctorReference.title}<br />
+          ${sessionString.doctorReference.phone}</td>
+          <td>${sessionString.notes}</td>
+          <td>Notes created on: ${sessionString.createdAt}<br />
+          Doctor id: ${sessionString.doctorReference._id}<br />
+          Session id: ${sessionString._id}</td>
+        </tr>
+      </table>
+      `)
+      $("#show-data").append(showHTML).show() 
 }
 
 const onUpdateOneSessionSuccess = function (data) {
@@ -110,7 +151,7 @@ const onUpdateOneSessionSuccess = function (data) {
     <p>Session was updated successfully</p> 
   `)
   $("#message").append(showHTML).show()
-    .delay(2000).fadeOut()
+    .delay(1500).fadeOut()
 }
 
 const onDeleteOneSessionSuccess = function () {
@@ -120,7 +161,7 @@ const onDeleteOneSessionSuccess = function () {
     <br>
   `)
   $("#message").append(showHTML).show()
-    .delay(2000).fadeOut()
+    .delay(1500).fadeOut()
 }
 
   
@@ -133,7 +174,7 @@ const userAuthError = function(error) {
         <br>
       `)
       $("#message").append(showHTML).show()
-      .delay(2000).fadeOut()
+      .delay(1500).fadeOut()
   }
 }
 
@@ -146,7 +187,7 @@ const otherError = function () {
         <br>
       `)
       $("#message").append(showHTML).show()
-      .delay(2000).fadeOut()
+      .delay(1500).fadeOut()
   }
 // empty values from form fields
 // $("input[type=text], textarea").val("")
